314900d38d83b998b4dc9f11064aa066b46c1a76
Malik Zafar <mzafar@ubriety.com>
2025-01-09 19:24:24 +0000

    dev: fix version sorting in agreements/review route

--- src/Entity/Account.php
+++ src/Entity/Account.php
@@ -149,7 +149,10 @@ class Account
                 ON term.id = agreement.term_id
                 AND agreement.account_uid = '{$this->uid}'
                 WHERE agreement.term_id IS NULL
-                AND term.version = (SELECT MAX(version) FROM term as termInner WHERE termInner.name = term.name)";
+                AND CAST(term.version AS DECIMAL(8, 3)) = (
+                    SELECT MAX(CAST(termInner.version AS DECIMAL(8, 3))) 
+                    FROM term AS termInner 
+                    WHERE termInner.name = term.name)";
 
         return $connection->fetchFirstColumn($sql);
     }
